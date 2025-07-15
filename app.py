da265dbbf3cedfbce87201b22a9612e52ecadc0b
apriluu <abriltc04@gmail.com>
2025-07-15 11:35:20 +0200

    clau11

--- app.py
+++ app.py
@@ -21,28 +21,24 @@ def ocr_space_file(image_bytes):
 
 def extreu_dades(text):
 
-    empresa = re.search(r'([A-ZÀ-Ú\s]{5,}SL)', text)
+    empresa = re.search(r'(?i)([A-ZÀ-Ú0-9\s\.,&-]{3,}S\.?L\.?)', text)
     data = re.search(r'Data[:\s]*(\d{2}/\d{2}/\d{4})', text)
 
     línies = text.splitlines()
     import_final = None
 
-    for i, línia in enumerate(línies):
-        if "TOTAL" in línia.upper():
-            tots_els_imports = []
-            for j in range(i - 1, max(i - 4, -1), -1):
-                possibles = re.findall(r'(\d+[.,]\d{2})', línies[j])
-                tots_els_imports.extend(possibles)
+    for línia in línies:
+        if re.search(r'(total|eurots?|a pagar|importe)', línia, re.IGNORECASE):
+            preus = re.findall(r'(\d+[.,]\d{2})', línia)
+            if preus:
+                import_final = max(preus,key=lambda x:float(x.replace(',','')))
+                break
 
+        if not import_final:
+            tots_els_imports = re.findall(r'(\d+[.,]\d{2})', text)
             if tots_els_imports:
                 import_final = max(tots_els_imports, key=lambda x: float(x.replace(',', '.')))
                 import_final = import_final.replace('.', ',')
-
-            if not import_final:
-                tots_els_imports = re.findall(r'(\d+[.,]\d{2})', text)
-                if tots_els_imports:
-                    import_final = max(tots_els_imports, key=lambda x: float(x.replace(',', '.')))
-                    import_final = import_final.replace('.', ',')
 
     return {
         "Empresa": empresa.group(0).strip() if empresa else "Desconeguda",
